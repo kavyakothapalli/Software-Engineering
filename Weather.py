8459a3f73dd2495d99518dda88348c3c89ee3e42
kavyakothapalli <113082437+kavyakothapalli@users.noreply.github.com>
2023-12-14 10:50:16 +0530

    Add files via upload

---
+++ Weather.py
@@ -0,0 +1,13 @@
+print("Welcome to weather prediction")
+T = float(input("Enter the temperature:"))
+H = float(input("Enter Humidity:"))
+wind =float(input("Enter wind speed:"))
+W=0.5*(T*T)-0.2*(H)+(0.1*wind)-15
+if W>300:
+    print("prediction:Sunny")
+elif W<=300 & W>200:
+    print("prediction:Cloudy")
+elif W<=200 & W>100:
+    print("prediction:Rainy")
+elif W<=100:
+    print("prediction:Stormy")
